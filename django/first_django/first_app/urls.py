e539cc4bf4cebf56a71d49e2542ec033465d1182
Huy Ngo <huyngopt1994@gmail.com>
2018-04-05 12:49:38 +0700

    add form for first_app

--- django/first_django/first_app/urls.py
+++ django/first_django/first_app/urls.py
@@ -4,5 +4,6 @@ from first_app import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('user', views.user, name='user')
+    path('user', views.user, name='user'),
+    path('form',views.form_name_view, name='form')
 ]
--- django/first_django/first_app/views.py
+++ django/first_django/first_app/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from first_app.models import Topic,Webpage, AccessRecord, User
-
+from . import forms
 # Create your views here.
 
 def index(request):
 	webpages_list = AccessRecord.objects.order_by('date')
 	date_dict = {'access_records': webpages_list}
 	return render(request,'first_app/index.html',context=date_dict)
+
+def form_name_view(request):
+	form = forms.FormName()
+	if request.method == 'POST':
+		form = forms.FormName(request.POST)
+		if form.is_valid():
+			# DO SOMETHING CODE
+			print("VALIDATION SUCCESS!")
+			print ("NAME %s" % form.cleaned_data['name'])
+			print ("EMAIL %s" % form.cleaned_data['email'])
+			print ("TEXT %s" % form.cleaned_data['text'])
+
+	return render(request,'first_app/form_page.html',context={'form':form})
 
 def user(request):
 	user_list = User.objects.order_by('first_name')
